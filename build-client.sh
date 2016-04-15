80d429ae43c21cfbce5a3df2f6f40d017abb82b4
sparlant <sandra.parlant@zenika.com>
2016-04-15 10:20:17 +0200

    script de build

--- build-client.sh
+++ build-client.sh
@@ -3,5 +3,3 @@
 docker build -f Dockerfile.build -t zenika/liquid-democracy-build .
 docker run --rm -v $PWD/dist:/dist zenika/liquid-democracy-build
 docker build -t zenika/liquid-democracy .
-docker rm -f liquid-democracy-front
-docker run -d --name liquid-democracy-front --link liquiddemocracyapi_liquidDemocracyApi_1:api -p 80:80 zenika/liquid-democracy
