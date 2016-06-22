e542a17e711b9bfa287a751fad5e2a8cf2f32a80
kepennar <kevin.pennarun@gmail.com>
2016-06-22 12:00:21 +0200

    fancier login button

--- src/app/components/App/Portal/index.js
+++ src/app/components/App/Portal/index.js
@@ -1,31 +1,42 @@
 import React, { Component, PropTypes } from 'react';
-import {googleResponsType as responseType,
-        googleClientId as clientId,
-        googleRedirectUri as redirectUri,
-        googleScope as scope} from '../../../config/api';
 import {
-  Button,
+  googleResponsType as responseType,
+  googleClientId as clientId,
+  googleRedirectUri as redirectUri,
+  googleScope as scope
+} from '../../../config/api';
+import {
+  Grid,
+  Row,
+  Col,
+  FormGroup,
+  FormControl,
 } from 'react-bootstrap';
 
-
-
-//import './index.scss';
+import './index.scss';
 
 export default class Portal extends Component {
 
-
-
-  getGoogleUrl(){
+  getGoogleUrl() {
     return '/signin/google';
   }
 
   render() {
     return (
-      <div>
-        <form method="POST" action={this.getGoogleUrl()}>
-          <input type="submit" value="Authenticate by google"/>
-        </form>
-      </div>
+      <Grid>
+        <Row>
+          <Col xs={6} xsOffset={3}>
+            <form method="POST" action={this.getGoogleUrl()}>
+              <FormGroup bsSize="large" >
+                <FormControl type="submit"
+                  value="Authenticate by google"
+                  className="google-button"
+                />
+              </FormGroup>
+            </form>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
