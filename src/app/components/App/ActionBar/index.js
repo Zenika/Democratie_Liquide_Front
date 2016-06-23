1d75979fc73ee434e82cc7b8e5c285fc6cdcf52d
kepennar <kevin.pennarun@gmail.com>
2016-06-23 11:28:28 +0200

    restyle action bar

--- src/app/components/App/ActionBar/index.js
+++ src/app/components/App/ActionBar/index.js
@@ -11,6 +11,8 @@ import {
   Tooltip
 } from 'react-bootstrap';
 
+import './index.scss';
+
 export default class ActionBar extends Component {
 
   constructor(props) {
@@ -38,67 +40,69 @@ export default class ActionBar extends Component {
     const { selectedLabel } = this.state;
 
     return (
-      <div>
-        <row>
-          <Col xs={6}>
-              Catégorie :
-              <DropdownButton title={this.state.selectedLabel} id="bg-nested-dropdown" >
-                <MenuItem eventKey="ALL" onSelect={key => this.props.selectCategory(key)}>ALL</MenuItem>
-                  {
-                    this.props.categories.map((c, i) =>
-                      <MenuItem key={i} eventKey={c.uuid} onSelect={key => this.selectCategory(key)}>
-                          <Row>
+      <Row className="action-bar">
+        <Col xs={6}>
+            Catégorie :
+            <DropdownButton title={this.state.selectedLabel} id="bg-nested-dropdown" >
+              <MenuItem eventKey="ALL" onSelect={key => this.props.selectCategory(key)}>ALL</MenuItem>
+                {
+                  this.props.categories.map((c, i) =>
+                    <MenuItem key={i} eventKey={c.uuid} onSelect={key => this.selectCategory(key)}>
+                        <Row>
 
-                            <Col xs={12} md={8}>
-                              {c.title}
-                            </Col>
+                          <Col xs={12} md={8}>
+                            {c.title}
+                          </Col>
 
-                            <Col xs={9} md={6}>
-                              <OverlayTrigger
-                                placement="top"
-                                overlay={<Tooltip id="Déléguer">Déléguer</Tooltip>}
+                          <Col xs={9} md={6}>
+                            <OverlayTrigger
+                              placement="top"
+                              overlay={<Tooltip id="Déléguer">Déléguer</Tooltip>}
+                            >
+                              <Button
+                                className="action-button"
+                                onClick={(e) => this.props.showCategoryDelegate(true)}
                               >
-                                <Button
-                                  className="action-button"
-                                  onClick={(e) => this.props.showCategoryDelegate(true)}
-                                >
-                                  <Glyphicon
-                                    glyph="transfer"
-                                  />
-                                </Button>
-                              </OverlayTrigger>
-                            </Col>
+                                <Glyphicon
+                                  glyph="transfer"
+                                />
+                              </Button>
+                            </OverlayTrigger>
+                          </Col>
 
-                            <Col xs={9} md={6}>
-                              <OverlayTrigger
-                                placement="top"
-                                overlay={<Tooltip id="Supprimer">Supprimer la délégation</Tooltip>}
+                          <Col xs={9} md={6}>
+                            <OverlayTrigger
+                              placement="top"
+                              overlay={<Tooltip id="Supprimer">Supprimer la délégation</Tooltip>}
+                            >
+                              <Button
+                                className="action-button"
+                                onClick={(e) => this.props.onRemoveDelegation()}
                               >
-                                <Button
-                                  className="action-button"
-                                  onClick={(e) => this.props.onRemoveDelegation()}
-                                >
-                                  <Glyphicon glyph="remove" />
-                                </Button>
-                              </OverlayTrigger>
-                            </Col>
+                                <Glyphicon glyph="remove" />
+                              </Button>
+                            </OverlayTrigger>
+                          </Col>
 
-                          </Row>
-                      </MenuItem>
-                    )
-                  }
-              </DropdownButton>
-          </Col>
-          <Col xs={6}>
-              <Button onClick={() => this.props.manageNewSubject(true)}>
-                <Glyphicon glyph="plus" /> Créer un sujet
-              </Button>
-              <Button onClick={() => this.props.manageNewCategory(true)}>
-                <Glyphicon glyph="plus" /> Créer une nouvelle catégorie
-              </Button>
-          </Col>
-        </row>
-      </div>
+                        </Row>
+                    </MenuItem>
+                  )
+                }
+            </DropdownButton>
+        </Col>
+        <Col xs={3}>
+          <Button className="pull-right"
+            onClick={() => this.props.manageNewSubject(true)}
+          >
+            <Glyphicon glyph="plus" /> Créer un sujet
+          </Button>
+        </Col>
+        <Col xs={3}>
+          <Button onClick={() => this.props.manageNewCategory(true)}>
+            <Glyphicon glyph="plus" /> Créer une nouvelle catégorie
+          </Button>
+        </Col>
+      </Row>
     );
 
   }
