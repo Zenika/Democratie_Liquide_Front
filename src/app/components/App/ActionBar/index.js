20f834110ffb6080889fe819c0555f8ec2b8f399
Valentin Bailleul <vlt.bailleul@gmail.com>
2016-10-14 14:18:28 +0200

    Change appearance of ActionBar

--- src/app/components/App/ActionBar/index.js
+++ src/app/components/App/ActionBar/index.js
@@ -18,33 +18,34 @@ export default class ActionBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLabel: "ALL",
+      selectedCategory: "ALL",
     };
   }
 
   selectCategory(key) {
-    if (key === "ALL") {
-      this.setState({
-        selectedLabel: "ALL",
-      });
-    } else {
-      this.setState({
-        selectedLabel: this.props.categories.find(c => c.uuid === key).title,
-      });
-    }
-
+    console.log('app/components/App/ActionBar@selectCategory', key);
+    this.setState({ selectedCategory: key === 'ALL' ? 'ALL' : this.props.categories.find(c => c.uuid === key).title });
     this.props.selectCategory(key);
   }
 
   render() {
-    const { selectedLabel } = this.state;
+    const { selectedCategory } = this.state;
 
     return (
       <Row className="action-bar">
-        <Col xs={12} md={6}>
-          Catégorie :
-            <DropdownButton title={this.state.selectedLabel} id="bg-nested-dropdown" >
-              <MenuItem eventKey="ALL" onSelect={key => this.props.selectCategory(key)}>ALL</MenuItem>
+
+        <Col xs={6} md={6}>
+          <Button
+            onClick={() => this.props.manageNewSubject(true)}
+          >
+            <Glyphicon glyph="plus"/> Créer un sujet
+          </Button>
+        </Col>
+
+        <Col xs={6} md={6}>
+          Catégorie : &nbsp;
+            <DropdownButton title={this.state.selectedCategory} id="bg-nested-dropdown" >
+              <MenuItem eventKey="ALL" onSelect={key => this.selectCategory(key)}>ALL</MenuItem>
                 {
                   this.props.categories.map((c, i) =>
                     <MenuItem key={i} eventKey={c.uuid} onSelect={key => this.selectCategory(key)}>
@@ -89,19 +90,14 @@ export default class ActionBar extends Component {
                   )
                 }
             </DropdownButton>
+
+            <OverlayTrigger placement="top" overlay={<Tooltip id="Créer">Créer</Tooltip>} >
+              <Button onClick={() => this.props.manageNewCategory(true)}>
+                <Glyphicon glyph="plus"/>
+              </Button>
+            </OverlayTrigger>
         </Col>
-        <Col xs={6} md={3}>
-          <Button
-            onClick={() => this.props.manageNewSubject(true)}
-          >
-            <Glyphicon glyph="plus"/> Créer un sujet
-          </Button>
-        </Col>
-        <Col xs={6} md={3}>
-          <Button onClick={() => this.props.manageNewCategory(true)}>
-            <Glyphicon glyph="plus"/> Créer une catégorie
-          </Button>
-        </Col>
+
       </Row>
     );
 
