c09033ad9e1d3e58816730e8bf58c064702f0414
Mathieu POUSSE <m.mathieu.pousse@gmail.com>
2016-12-22 18:06:44 +0100

    rename docker images

--- build-client.sh
+++ build-client.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-docker build -f Dockerfile.build -t zenika/liquid-democracy-build .
-docker run --rm -v $PWD/dist:/dist zenika/liquid-democracy-build
-docker build -t zenika/liquid-democracy .
+docker build -f Dockerfile.build -t zenika/agora-front-build .
+docker run --rm -v $PWD/dist:/dist zenika/agora-front-build
+docker build -t zenika/agora-front .
